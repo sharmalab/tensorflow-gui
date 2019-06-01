7aa0647efefaa8930700fd429e6730476e95cce7
Vikas Gola <vikasgola2015@gmail.com>
2019-06-02 02:12:02 +0530

    add List of all created projects in user module

--- src/modules/user/user.js
+++ src/modules/user/user.js
@@ -1,6 +1,9 @@
 const {
     ipcRenderer
 } = require('electron')
+const swal = require('sweetalert');
+const fs = require('fs')
+const print = console.log;
 
 function loadPage(page_path) {
     $("#main-content").html('');
@@ -8,27 +11,100 @@ function loadPage(page_path) {
 }
 
 $("#user-create-project-button").click(() => {
-    // title = "Sad";
-    // val = "ASD";
-    // args = {
-    //     title,
-    //     val
-    // }
-    // ipcRenderer.on('prompt-reply', (event, result) => {
-    //     console.log("asdsd",result)
-    // })
+    swal({
+        text: "Project Name",
+        title: "Create New Project",
+        content: "input",
+        buttons: ["Cancel", "Next"],
+    }).then((value) => {
+        if (value == "") {
+            swal("Error", "Projet Name can't be empty.", "error");
+        } else if (value) {
+            let dir = value.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+            let basepath = process.cwd() + "/testing/Projects/";
+            if (!fs.existsSync(basepath)) {
+                fs.mkdirSync(basepath);
+            }
 
-    // ipcRenderer.sendSync('prompt', args)
+            if (!fs.existsSync(basepath + dir)) {
+                fs.mkdirSync(basepath + dir, err => {
+                    if (err) {
+                        print("Error creating folder", err);
+                    }
+                });
+                swal({
+                    text: "Project Details",
+                    title: "Create New Project",
+                    content: "input",
+                    buttons: ["Cancel", "Create"],
+                }).then(value => {
+                    if (value == "") {
+                        swal("Error", "Project Details can't be empty.");
+                    } else if (value) {
+                        let data = {
+                            name: dir,
+                            details: value,
+                            time: new Date(Date.now()).toString()
+                        };
+                        fs.writeFile(basepath + dir + "/info.json", JSON.stringify(data), 'utf-8', err => {
+                            if (err) {
+                                print("Error writing file", err);
+                            } else {
+                                swal("Create New Project", "Project created successfully.", "success");
+                            }
+                        });
+                    }
+                });
+            } else {
+                swal("Error", "Project with this name already exists.", "error");
+            }
+        }
+    });
+});
 
-    // TODO: call window for craeting project
-    console.log("prject created.")
-})
+function getDirectories(path) {
+    return fs.readdirSync(path).filter(function (file) {
+        return fs.statSync(path + '/' + file).isDirectory();
+    });
+}
 
+$(document).ready(() => {
+    let basepath = process.cwd() + "/testing/Projects/";
+    let dirlist = getDirectories(basepath)
 
-$("#first-project-gui").click(() => {
-    loadPage("draw/draw.html")
-})
+    for (let dir in dirlist) {
+        fs.readFile(basepath + dirlist[dir] + "/info.json", (err, fileData) => {
+            if (err) {
+                return print("Error in reading all projects", err)
+            }
+            try {
+                const object = JSON.parse(fileData);
+                $("#user-projects-card-row").append(`
+                <div class="col-sm-*">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${object.name}</h5>
+                            <p class="card-text">${object.details}</p>
+                            <button type="button" class="btn btn-primary openbuttons">
+                                Open Project
+                            </button>
+                            </div>
+                        </div>
+                    </div>
+                `);
 
-// $("#first-project-code-editor").click(() => {
-//     loadPage("codeeditor/codeeditor.html")
-// })
+                // $(".openbuttons").click(() => {
+                //     print(;
+                //     loadPage("draw/draw.html")
+                // });
+            } catch (err) {
+                return print("Error in reading all projects", err)
+            }
+        })
+    }
+
+    function openProject(object){
+        print(object);
+        print(JSON.parse(object))
+    }
+});
